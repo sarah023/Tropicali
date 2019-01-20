0f9912b54d725dbf88d0a3f080912e9706bfa380
Sarah Grundy <sarahgrundy023@gmail.com>
2019-01-20 18:36:20 +1030

    github deploy script added

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sourcemaps = require('gulp-sourcemaps')
 var browserSync = require('browser-sync').create()
 
 var imagemin = require('gulp-imagemin')
+var ghpages = require('gh-pages')
 
 
 sass.compiler = require('node-sass')
@@ -56,6 +57,8 @@ gulp.task('watch', function () {
     gulp.watch('src/img/*', ['images'])
 })
 
-
+gulp.task('deploy', function () {
+    ghpages.publish('dist')
+})
 
 gulp.task('default', ['html', 'sass', 'fonts', 'images', 'watch'])
