48d9f8e8bfdc3944d9b90d14ea94ee8ed3962aa8
Sarah Grundy <sarahgrundy023@gmail.com>
2019-01-20 23:03:43 +1030

    site ready to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,43 @@
 var gulp = require('gulp')
-var sass = require('gulp-sass')
-var cleanCss = require('gulp-clean-css')
-var sourcemaps = require('gulp-sourcemaps')
 
+//css
+var cleanCss = require('gulp-clean-css')
+var postcss = require('gulp-postcss')
+var sourcemaps = require('gulp-sourcemaps')
+var concat = require('gulp-concat')
+
+//browser refresh
 var browserSync = require('browser-sync').create()
 
+//images
 var imagemin = require('gulp-imagemin')
+
+//github
 var ghpages = require('gh-pages')
 
 
-sass.compiler = require('node-sass')
 
-gulp.task('sass', function () {
-    // we want to run sass --watch css/app.scss app.css
-    return gulp.src('src/css/app.scss')
+gulp.task('css', function () {
+    
+    return gulp.src([
+        'src/css/reset.css',
+        'src/css/typography.css',
+        'src/css/app.css'
+    ])
         .pipe(sourcemaps.init())
-        .pipe(sass())
+        .pipe(
+            postcss([
+                require('autoprefixer'),
+                //add post css extras
+                require('postcss-preset-env')({
+                    //allow more experimental versions
+                    stage: 1,
+                    //allow different browser versions
+                    browsers: ['IE 11', 'last 2 versions']
+                 })
+            ])
+        )
+        .pipe(concat('app.css'))
         .pipe(
             cleanCss({
                 compatibility: 'ie8'
@@ -52,7 +74,7 @@ gulp.task('watch', function () {
     })
 
     gulp.watch('src/*.html', ['html']).on('change', browserSync.reload)
-    gulp.watch('src/css/app.scss', ['sass'])
+    gulp.watch('src/css/*.css', ['css'])
     gulp.watch('src/fonts/*', ['fonts'])
     gulp.watch('src/img/*', ['images'])
 })
@@ -61,4 +83,4 @@ gulp.task('deploy', function () {
     ghpages.publish('dist')
 })
 
-gulp.task('default', ['html', 'sass', 'fonts', 'images', 'watch'])
+gulp.task('default', ['html', 'css', 'fonts', 'images', 'watch'])
